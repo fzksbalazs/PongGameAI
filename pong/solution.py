8eb63a103f0bbc9716b005aca647d66cfc7f65e9
fzksbalazs <fzksbalazs@gmail.com>
2024-04-15 16:33:06 +0200

    Téglalapok létrehozása, mozgás probálkozás

--- pong/solution.py
+++ pong/solution.py
@@ -44,6 +44,28 @@ class Ball:
         self.x_vel *= -1
 
 
+class Paddle:
+    COLOR = WHITE
+    VEL = 4
+    
+    def __init__(self, x,y, width, height):
+        self.x = self.original_x = x
+        self.y = self.original_y =  y
+        self.width = width
+        self.height = height
+        
+        
+    def draw (self,win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+        
+    def move (self, up=True):
+        if up:
+            self.y -= self.VEL    
+        else:
+            self.y += self.VEL   
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
         
         
 def draw(win, paddles, ball, left_score, right_score):
