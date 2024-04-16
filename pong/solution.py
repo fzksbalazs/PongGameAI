1f8a8b23dfca53645f98d30efca6b614fdef9772
fzksbalazs <fzksbalazs@gmail.com>
2024-04-16 16:33:51 +0200

    Főbb funkciók létrehozása, ütközéssel kisérlet

--- pong/solution.py
+++ pong/solution.py
@@ -89,6 +89,34 @@ def draw(win, paddles, ball, left_score, right_score):
     ball.draw(win)
     pygame.display.update()
     
+def handle_collision(ball, left_paddle, right_paddle):
+    if ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_vel *=-1    
+        
+    if ball.x_vel < 0:
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+                
+                middle_y = left_paddle.y + left_paddle.height/ 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height /2) / ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = -1 * y_vel
+        
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
+                
+                middle_y = right_paddle.y + right_paddle.height/ 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (right_paddle.height /2) / ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = -1 * y_vel
+    
 
    
    
