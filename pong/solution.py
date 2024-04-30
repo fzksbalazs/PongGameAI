c66fd43fd11ca06a6a732457843312910a1aa43c
fzksbalazs <fzksbalazs@gmail.com>
2024-04-30 15:04:26 +0200

    Labda kész

--- pong/solution.py
+++ pong/solution.py
@@ -13,6 +13,7 @@ WHITE = (225,225,225)
 BLACK = (0,0,0)
 
 PADDLE_WIDTH , PADDLE_HEIGHT = 20,100
+BALL_RADIUS = 7
 
 
 
@@ -24,11 +25,15 @@ class Ball:
         self.x = x
         self.y = y
         self.radius = radius
-        self.x_vel = MAX_VEL
+        self.x_vel = self.MAX_VEL
         self.y_vel = 0
         
     def draw(self, win):
-        pygame.draw.circle(win, self.COLOR, self.)
+        pygame.draw.circle(win, self.COLOR,(self.x, self.y), self.radius)
+        
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
 
 
 class Paddle:
@@ -52,7 +57,7 @@ class Paddle:
             self.y += self.VEL    
         
         
-def draw(win, paddles):
+def draw(win, paddles, ball):
     win.fill(BLACK)
     
     for paddle in paddles:
@@ -64,6 +69,7 @@ def draw(win, paddles):
             continue
         pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
     
+    ball.draw(win)
     pygame.display.update()
     
    
@@ -86,11 +92,12 @@ def main():
     
     left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT )
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH , HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT )
+    ball = Ball(WIDTH//2, HEIGHT//2, BALL_RADIUS )
     
     
     while run:
         clock.tick(FPS)
-        draw(WIN, [left_paddle, right_paddle])
+        draw(WIN, [left_paddle, right_paddle], ball)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -99,6 +106,8 @@ def main():
             
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle, right_paddle)
+        
+        ball.move()
         
         
     pygame.quit()
